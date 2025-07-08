d5257dae9bf0af47c137d18c8a940f76f4d22d4f
Randall Flagg <O@G>
2025-07-08 21:54:27 +0300

    Unifying the project

---
+++ Solution Items/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCulture("")] //TODO: Move this to csproj file?
+[assembly: AssemblyTrademark("")] //TODO: Move this to csproj file?
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+//warning CA1824: Mark assemblies with NeutralResourcesLanguageAttribute
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+//[assembly: Guid("2d31fb81-1e79-6c20-9e92-375fe7ac2b5f")]
